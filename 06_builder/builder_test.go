a93ad36a5e78f03ffa8298c5c90682b304a59e72
Senghoo Kim <shkdmb@gmail.com>
2016-03-30 22:20:02 +0800

    update builder mode

--- 06_builder/builder_test.go
+++ 06_builder/builder_test.go
@@ -2,26 +2,6 @@ package builder
 
 import "testing"
 
-type Builder1 struct {
-	result string
-}
-
-func (b *Builder1) Part1() {
-	b.result += "1"
-}
-
-func (b *Builder1) Part2() {
-	b.result += "2"
-}
-
-func (b *Builder1) Part3() {
-	b.result += "3"
-}
-
-func (b *Builder1) GetResult() string {
-	return b.result
-}
-
 func TestBuilder1(t *testing.T) {
 	builder := &Builder1{}
 	director := NewDirector(builder)
@@ -30,26 +10,6 @@ func TestBuilder1(t *testing.T) {
 	if res != "123" {
 		t.Fatalf("Builder1 fail expect 123 acture %s", res)
 	}
-}
-
-type Builder2 struct {
-	result int
-}
-
-func (b *Builder2) Part1() {
-	b.result += 1
-}
-
-func (b *Builder2) Part2() {
-	b.result += 2
-}
-
-func (b *Builder2) Part3() {
-	b.result += 3
-}
-
-func (b *Builder2) GetResult() int {
-	return b.result
 }
 
 func TestBuilder2(t *testing.T) {
