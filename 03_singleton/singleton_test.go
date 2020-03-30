ef73f66facdef8214afdcefc250853f58e6a5b53
razertory <razertory@razertorys-Mac-mini.local>
2020-03-30 09:56:22 +0800

    update: 拆分出并发的单例测试

--- 03_singleton/singleton_test.go
+++ 03_singleton/singleton_test.go
@@ -5,22 +5,31 @@ import (
 	"testing"
 )
 
-const count = 100
+const parCount = 100
 
 func TestSingleton(t *testing.T) {
+	ins1 := GetInstance()
+	ins2 := GetInstance()
+	if ins1 != ins2 {
+		t.Fatal("instance is not equal")
+	}
+}
+
+func TestParallelSingleton(t *testing.T) {
 	wg := sync.WaitGroup{}
-	wg.Add(count)
-	instances := [count]*Singleton{}
-	for i := 0; i < count; i++ {
+	wg.Add(parCount)
+	instances := [parCount]*Singleton{}
+	for i := 0; i < parCount; i++ {
 		go func(index int) {
 			instances[index] = GetInstance()
 			wg.Done()
 		}(i)
 	}
 	wg.Wait()
-	for i := 1; i < count; i++ {
+	for i := 1; i < parCount; i++ {
 		if instances[i] != instances[i-1] {
 			t.Fatal("instance is not equal")
 		}
 	}
 }
+
