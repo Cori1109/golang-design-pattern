09ca01f16961f800293389f65dffb1f84f78c5a1
pomo <m13143378602@163.com>
2020-07-04 14:41:28 +0800

    fix parallel test

--- 03_singleton/singleton_test.go
+++ 03_singleton/singleton_test.go
@@ -16,15 +16,18 @@ func TestSingleton(t *testing.T) {
 }
 
 func TestParallelSingleton(t *testing.T) {
+	start := make(chan struct{})
 	wg := sync.WaitGroup{}
 	wg.Add(parCount)
 	instances := [parCount]*Singleton{}
 	for i := 0; i < parCount; i++ {
 		go func(index int) {
+			<-start
 			instances[index] = GetInstance()
 			wg.Done()
 		}(i)
 	}
+	close(start)
 	wg.Wait()
 	for i := 1; i < parCount; i++ {
 		if instances[i] != instances[i-1] {
